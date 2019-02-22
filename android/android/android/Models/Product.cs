7ef7013e157d34b48d0c4f207854f11d14c3825c
fulara <ntszar@gmail.com>
2019-02-22 18:19:09 +0100

    MAP VIEWgit add --allgit add --allgit add --all

---
+++ android/android/android/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace android.Models
+{
+    class Product
+    {
+        Category category;
+        String name;
+        String location;
+        int count;
+
+        bool kupione;
+    }
+
+    struct Category
+    {
+        //A,B,C,D
+        String representation;
+    }
+}
---
+++ android/android/android/Views/MapPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace android.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+
+            MapImage.Source = MapDisplay();
+            Container.Children.Add(create());
+        }
+
+        ImageSource MapDisplay()
+        {
+            //make this async someday?
+            return ImageSource.FromUri(new Uri("https://www.thegamecrafter.com/overlays/squareboard.png"));
+        }
+
+        private Label create()
+        {
+            var x = new Label
+            {
+                Text = "hello World",
+            };
+
+            AbsoluteLayout.SetLayoutBounds(x, new Rectangle(.5, .2, .5, .1));
+            AbsoluteLayout.SetLayoutFlags(x, AbsoluteLayoutFlags.All);
+
+            return x;
+        }
+    }
+}
