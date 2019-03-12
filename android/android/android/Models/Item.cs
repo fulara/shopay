3b438423cca3e993dffa363be0591bf99285e00b
Guciax <piotrek816@wp.pl>
2019-03-12 21:55:29 +0100

    ItemsPage: fixed switch zone, added description and quantity

--- android/android/android/Models/Item.cs
+++ android/android/android/Models/Item.cs
@@ -9,6 +9,7 @@ namespace android.Models
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+        public int Quantity { get; set; }
 
         public bool Switch { get
             {
